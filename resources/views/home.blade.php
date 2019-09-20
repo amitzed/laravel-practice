68af728b6024754dba1aae81fc6077aec5906a03
Amit <amitzmn@gmail.com>
2019-09-19 23:10:59 -0400

    Put html boilerplate in Home blade file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Laravel Practice</title>
+  </head>
+  <body>
+    <h1>Home Page</h1>
+
+  </body>
+</html>
